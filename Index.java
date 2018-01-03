05b68cc55a78dc4d952621d4ebca73faa5369226
pasyet <farapasyetnabila@gmail.com>
2018-01-03 11:21:13 +0700

    Belajar Tipe Data

---
+++ Index.java
@@ -0,0 +1,12 @@
+public class Index {
+    public static void main (String[] args){
+        System.out.println("Angka: " +TipeData.angka);
+        System.out.println("Nama: " +TipeData.nama);
+        System.out.println("Huruf depan nama " +TipeData.var);
+        System.out.println("Apa: " +TipeData.aktif);
+        System.out.println("Pecahan Tunggal: " +TipeData.pecahanT);
+        System.out.println("Pecahan Ganda: " +TipeData.pecahanG);
+        System.out.println("Nilai: " +TipeData.nilai);
+    }
+
+}
---
+++ TipeData.java
@@ -0,0 +1,9 @@
+public class TipeData{
+    public static final Integer angka = 10;
+    public static final String nama = "Nabila";
+    public static final Character var = 'N';
+    public static final Boolean aktif = true;
+    public static final float pecahanT = 0.2f;
+    public static final Double pecahanG = 0.22;
+    public static final Byte nilai = 127;
+}
